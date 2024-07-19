8be67a05286ce9e8c5276a01f3d8dee0dcad2071
Sandrimar Gibleski <sandrimarjose@outlook.com>
2024-07-19 11:13:51 -0300

    Create task

--- backend/src/main/java/com/sandrimar/backend/controllers/TaskController.java
+++ backend/src/main/java/com/sandrimar/backend/controllers/TaskController.java
@@ -1,16 +1,31 @@
 package com.sandrimar.backend.controllers;
 
+import com.sandrimar.backend.dto.TaskRequestDTO;
+import com.sandrimar.backend.dto.TaskResponseDTO;
 import com.sandrimar.backend.services.TaskService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin
 public class TaskController {
 
     private final TaskService service;
 
     public TaskController(TaskService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskRequestDTO body) {
+        TaskResponseDTO createdTask = service.createTask(body);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(createdTask.id()).toUri();
+        return ResponseEntity.created(uri).body(createdTask);
     }
 }
--- backend/src/main/java/com/sandrimar/backend/services/TaskService.java
+++ backend/src/main/java/com/sandrimar/backend/services/TaskService.java
@@ -1,7 +1,13 @@
 package com.sandrimar.backend.services;
 
+import com.sandrimar.backend.dto.TaskRequestDTO;
+import com.sandrimar.backend.dto.TaskResponseDTO;
+import com.sandrimar.backend.model.Task;
 import com.sandrimar.backend.repositories.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 @Service
 public class TaskService {
@@ -10,5 +16,14 @@ public class TaskService {
 
     public TaskService(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    public TaskResponseDTO createTask(TaskRequestDTO body) {
+        if (LocalDate.now().getDayOfWeek().equals(DayOfWeek.SATURDAY)
+                || LocalDate.now().getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+            throw new IllegalStateException("Tasks can only be created during weekdays");
+        }
+        Task createdTask = repository.save(new Task(body));
+        return new TaskResponseDTO(createdTask);
     }
 }
